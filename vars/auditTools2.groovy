d326e758b483624af8f8898224213e46b956ad1c
Ng Yew Kong <ngyewkong1996@gmail.com>
2024-02-16 10:54:03 +0800

    update groovy file

--- vars/auditTools2.groovy
+++ vars/auditTools2.groovy
@@ -1,8 +1,7 @@
 def call(Map config) {
     node {
         // echo and print out the message passed in 
-        echo "${config.message}"
-        echo "Add some processing within the auditTools2 function call"
+        echo "${config.message}\n... Add some processing within the auditTools2 function call"
         sh '''
             git version
             java -version
